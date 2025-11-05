6acd3a7b053629f145207c8c71d2102f882b19bf
ArmandoBeriX <158400084+ArmandoBeriX@users.noreply.github.com>
2025-11-05 05:23:55 -0900

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -1,8 +1,7 @@
-// app.config.ts
 import { defineConfig } from "@solidjs/start/config";
 
 export default defineConfig({
   server: {
-    preset: "static"  // ← Esto genera archivos estáticos correctos
+    preset: "node-server"
   }
 });
